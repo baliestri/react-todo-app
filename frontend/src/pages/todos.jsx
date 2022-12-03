10eb4b6f841d81983980f32c289bdadd8e540f05
Bruno Sales <me@baliestri.dev>
2022-12-03 13:37:43 -0300

    feat(frontend): add blank todos page

---
+++ frontend/src/pages/todos.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/layout/header";
+
+const Todos = () => {
+  return (
+    <>
+      <Header title="Tarefas" subtitle="Criar" />
+
+      <h1>Todos</h1>
+
+      <Header title="Tarefas" subtitle="Listar" />
+
+      <h1>Todos</h1>
+    </>
+  );
+};
+
+export default Todos;
