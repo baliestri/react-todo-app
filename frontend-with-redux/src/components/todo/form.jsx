742c5464de6e74e523b83887162ff5d501b3e52d
Bruno Sales <me@baliestri.dev>
2022-12-08 14:21:24 -0300

    feat(frontend-with-redux): connect `TodoForm` implementation

--- frontend-with-redux/src/components/todo/form.jsx
+++ frontend-with-redux/src/components/todo/form.jsx
@@ -1,7 +1,11 @@
 import { Box, IconButton, Stack, TextField } from "@mui/material";
 import { Add as AddIcon } from "@mui/icons-material";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
-const TodoForm = ({}) => {
+import { onAdd, onDescriptionChange } from "../../common/store/actions/todo";
+
+const TodoForm = ({ description, onAdd, onDescriptionChange }) => {
   return (
     <Box sx={{ padding: "2rem" }}>
       <Stack direction="row" spacing={2} mb={2}>
@@ -10,8 +14,21 @@ const TodoForm = ({}) => {
           variant="outlined"
           size="small"
           sx={{ flex: 1 }}
+          value={description}
+          onChange={onDescriptionChange}
+          onKeyUp={(event) => {
+            if (event.key === "Enter") {
+              event.preventDefault();
+              onAdd();
+            }
+
+            if (event.key === "Escape") {
+              event.preventDefault();
+              event.target.value = "";
+            }
+          }}
         />
-        <IconButton aria-label="adicionar" onClick={() => {}}>
+        <IconButton aria-label="adicionar" onClick={onAdd}>
           <AddIcon />
         </IconButton>
       </Stack>
@@ -19,4 +36,11 @@ const TodoForm = ({}) => {
   );
 };
 
-export default TodoForm;
+const mapStateToProps = (state) => ({
+  description: state.todo.description,
+});
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ onAdd, onDescriptionChange }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
