93bc7a126d938440b2bc1243d68b1f708665d936
Bruno Sales <me@baliestri.dev>
2022-12-08 14:20:56 -0300

    feat(frontend-with-redux): add store provider

--- frontend-with-redux/src/main.jsx
+++ frontend-with-redux/src/main.jsx
@@ -5,11 +5,15 @@ import "@fontsource/roboto/700.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./app";
+import Store from "./common/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={Store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
