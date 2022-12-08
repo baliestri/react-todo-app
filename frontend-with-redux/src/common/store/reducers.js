8c77cffeb5c347ec456b5d1f068003e3eccbc486
Bruno Sales <me@baliestri.dev>
2022-12-08 14:20:03 -0300

    feat(frontend-with-redux): add `rootReducer`

---
+++ frontend-with-redux/src/common/store/reducers.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+import todoReducer from "./reducers/todo";
+
+const rootReducer = combineReducers({
+  todo: todoReducer,
+});
+
+export default rootReducer;
